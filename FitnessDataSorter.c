bc9ba61aeb1382843548f1ee1972372a7f0469b1
AdamRobinson9 <146648076+AdamRobinson9@users.noreply.github.com>
2023-11-26 00:36:46 +0000

    Task 3 handling invalid file

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -15,78 +15,67 @@ FILE *file;
 FitnessData *data;
 int noOfRecords = 0;
 
-// This is your helper function. Do not change it in any way.
-// Inputs: character array representing a row; the delimiter character
-// Ouputs: date character array; time character array; steps character array
-void tokeniseRecord(const char *input, const char *delimiter,
-                    char *date, char *time, char *steps) {
-    // Create a copy of the input string as strtok modifies the string
-    char *inputCopy = strdup(input);
-    
-    // Tokenize the copied string
-    char *token = strtok(inputCopy, delimiter);
-    if (token != NULL) {        strcpy(date, token);
+// Function to tokenize a record
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
+    if (ptr != NULL) {
+        strcpy(date, ptr);
+        ptr = strtok(NULL, &delimiter);
+        if (ptr != NULL) {
+            strcpy(time, ptr);
+            ptr = strtok(NULL, &delimiter);
+            if (ptr != NULL) {
+                *steps = atoi(ptr);
+            }
+        }
     }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(time, token);
-    }
-    
-    token = strtok(NULL, delimiter);
-    if (token != NULL) {
-        strcpy(steps, token);
-    }
-    
-    // Free the duplicated string
-    free(inputCopy);
-
-                    }
-
+}
 
 int main() {
     char filename[100];
     printf("Input filename: ");
-            scanf("%s", filename);
-            file = fopen(filename, "r");
-            if (file == NULL) {
-                //Print error message if the file cannot be opened.
-                printf("Error: invalid file\n");
-                return 1;
-            }
-            else{
-                //Declare variables to store data read from the file.
-                int buffer_size = 25;
-                char line_buffer[buffer_size];
-                while (fgets(line_buffer, buffer_size, file) != NULL) {
-                    noOfRecords++;
-                }
-                data = malloc(noOfRecords * sizeof(FitnessData));
+    scanf("%s", filename);
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        //Print error message if the file cannot be opened.
+        printf("Error: invalid file\n");
+            return 1;
+    }
+    else{
+        //Declare variables to store data read from the file.
+        int buffer_size = 25;
+        char line_buffer[buffer_size];
+        while (fgets(line_buffer, buffer_size, file) != NULL) {
+            noOfRecords++;
+        }
+        data = malloc(noOfRecords * sizeof(FitnessData));
                 
-                rewind(file);
+        rewind(file);
 
-                int record = 0;
-                char stepCount[10];
-                bool validFile = true;
-                //Step through data, tokenise it and store it in the array of fitness data.
-                while (fgets(line_buffer, buffer_size, file) != NULL) {
-                    tokeniseRecord(line_buffer, ",", data[record].date, data[record].time, stepCount);
-                    if(data[record].date == NULL || data[record].time == NULL || stepCount == NULL){
-                        validFile = false;
-                    }
-                    data[record].steps = atoi(stepCount);
-                    record++;
-                }
-
-                if(validFile){
-                    printf("File successfully loaded.\n");
-                }
-                else{
-                    printf("Error: invalid file\n");
-                    return 1;
-                }
+        int record = 0;
+        //Step through data, tokenise it and store it in the array of fitness data.
+        while (fgets(line_buffer, buffer_size, file) != NULL) {
+            tokeniseRecord(line_buffer, ',', data[record].date, data[record].time, &data[record].steps);
+            record++;
+        }
                 
-                //Close the file.
-                fclose(file);
+        bool invalidData = false;
+        for (int i=0; i<noOfRecords; i++){
+            if(strlen(data[i].date) != 10 || strlen(data[i].time) != 5 || data[i].time < 0){
+                invalidData = true;
             }
+        }
+
+        if (invalidData){
+            printf("Error: invalid file\n");
+            return 1;
+        }
+        else{
+            for (int i=0; i<noOfRecords; i++){
+                printf("%s\t%s\t%d\n", data[i].date, data[i].time, data[i].steps);
+            }
+        }
+    }
+    //Close the file.
+    fclose(file);
 }
